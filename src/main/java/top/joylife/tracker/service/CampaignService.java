33cb24bba3b6ab25523a3186627bf89caf24a9d2
HemingWu <jianmo_o@163.com>
2018-12-09 21:29:45 +0800

    完善

--- src/main/java/top/joylife/tracker/service/CampaignService.java
+++ src/main/java/top/joylife/tracker/service/CampaignService.java
@@ -1,7 +1,6 @@
 package top.joylife.tracker.service;
 
 import com.github.pagehelper.PageInfo;
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
