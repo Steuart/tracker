5b948cb458474f8fcb18a32b9d79d3ebca71335d
steuart <jianmo_o@163.com>
2018-12-12 13:53:54 +0800

    bugfix

---
+++ src/main/java/top/joylife/tracker/config/ServerConfig.java
@@ -0,0 +1,20 @@
+package top.joylife.tracker.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServerConfig {
+
+    @Bean
+    public TomcatServletWebServerFactory containerFactory() {
+        return new TomcatServletWebServerFactory() {
+            protected void customizeConnector(Connector connector) {
+                super.customizeConnector(connector);
+                connector.setProperty("relaxedQueryChars", "|{}[]");
+            }
+        };
+    }
+}
